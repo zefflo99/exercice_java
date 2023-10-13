65374d16bceb96652883948f96b455f7ec629b64
kylian <kylianclerc@fedora>
2023-10-13 11:08:52 +0200

    exercice_java

--- src/main/java/ch/jobtrek/Csv.java
+++ src/main/java/ch/jobtrek/Csv.java
@@ -1,9 +1,15 @@
 package ch.jobtrek;
 
+import ch.jobtrek.sbb.Tunnel;
 import ch.jobtrek.sbb.Tunnelable;
 
+import java.io.File;
 import java.net.URI;
 import java.util.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
 
 public class Csv {
 
@@ -14,6 +20,11 @@ public class Csv {
      * @return A List of objects that implements the Tunnelable interface. You need to create this special class.
      */
     public static List<Tunnelable> importCSVfile(URI filePath) {
+        try (var file = Files.lines(Paths.get(filePath))){return file.skip(1)
+                .map(line -> line.split(";"))
+
+        }
+
         return List.of(); // Replace with your code here
     }
 
